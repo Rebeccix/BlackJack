92c8e12c4b466a7460440c0c22939814f2a9f348
Rebecca Ferreira <beccixzin@gmail.com>
2022-08-30 04:34:31 -0300

    refatorado

--- script.js
+++ script.js
@@ -1,127 +1,120 @@
-const cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
-
-const suit = ["♦️", "♥️", "♣️", "♠️"];
-
 const start = () => {
+  let result = 0;
+  let hand = [];
   let playerHand = [];
+  let playerPoint = 0;
   let opponentHand = [];
-  let totalPlayer = 0;
-  let totalOponent = 0;
-  let playerSuitToPush = [];
-  let opponentSuitToPush = [];
+  let opponentPoint = 0;
+  const drawCard = () => {
+    const cards = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K",
+    ];
+    const suitsArr = ["♦️", "♥️", "♣️", "♠️"];
+    number = cards[Math.floor(Math.random() * 13)];
+    suits = suitsArr[Math.floor(Math.random() * 4)];
 
-  //   looping to gerate/push the number, letters and suits
-  for (let i = 0; i <= 1; i++) {
-    playerRandomNum = Math.floor(Math.random() * 13);
-    playerRandomSuit = Math.floor(Math.random() * 4);
-    opponentRandomNum = Math.floor(Math.random() * 13);
-    opponentRandomSuit = Math.floor(Math.random() * 4);
-
-    // array with numbers and letters
-    playerHand.push(cards[playerRandomNum]);
-    opponentHand.push(cards[opponentRandomNum]);
-
-    // array with suit
-    playerSuitToPush.push(suit[playerRandomSuit]);
-    opponentSuitToPush.push(suit[opponentRandomSuit]);
-  }
-
-  // check duplicate
-  //   check dubplicates player hand
-  while (
-    playerHand[0] === playerHand[1] &&
-    playerSuitToPush[0] === playerSuitToPush[1]
-  ) {
-    playerHand = playerHand.slice(0, 0);
-    playerSuitToPush = playerSuitToPush.slice(0, 0);
-
-    let i = 0;
-    while (i <= 1) {
-      playerRandomNum = Math.floor(Math.random() * 13);
-      playerRandomSuit = Math.floor(Math.random() * 4);
-      playerHand.push(cards[playerRandomNum]);
-      playerSuitToPush.push(suit[playerRandomSuit]);
-      i++;
+    if (number === "A") {
+      result += 11;
+    } else if (number === "J" || number === "Q" || number === "K") {
+      result += 10;
+    } else {
+      result += +number;
     }
+
+    hand.push(number + suits);
+
+    return result, hand;
+  };
+
+  drawCard();
+  drawCard();
+
+  while (hand[0] === hand[1]) {
+    hand.slice(0, 0);
+    drawCard();
+    drawCard();
   }
 
-  //   check dubplicates opponent hand
-  while (
-    opponentHand[0] === opponentHand[1] &&
-    opponentSuitToPush[0] === opponentSuitToPush[1]
-  ) {
-    opponentHand = opponentHand.slice(0, 0);
-    opponentSuitToPush = opponentSuitToPush.slice(0, 0);
+  playerHand = hand;
 
-    let i = 0;
-    while (i <= 1) {
-      opponentRandomNum = Math.floor(Math.random() * 13);
-      opponentRandomSuit = Math.floor(Math.random() * 4);
-      opponentHand.push(cards[opponentRandomNum]);
-      opponentSuitToPush.push(suit[opponentRandomSuit]);
-      i++;
+  playerHand[0].includes("A") && playerHand[1].includes("A")
+    ? (playerPoint = 12)
+    : (playerPoint = result);
+
+  // --------------------------------
+  hand = hand.slice(0, 0);
+  result = 0;
+  drawCard();
+  drawCard();
+
+  while (hand[0] === hand[1]) {
+    hand.slice(0, 0);
+    drawCard();
+    drawCard();
+  }
+
+  opponentHand = hand;
+
+  opponentHand[0].includes("A") && opponentHand[1].includes("A")
+    ? (opponentPoint = 12)
+    : (opponentPoint = result);
+
+    // show your actual hand/points and the opponente hand/points
+  alert((`Player ${playerHand.toString().replaceAll(',', '')} \nTotal: ${playerPoint}`))
+  alert(`Oponente ${opponentHand[0].toString().replaceAll(',', '')}${opponentHand[1].toString().replaceAll(',', '')} \nTotal: ${opponentPoint}`)
+
+    while( opponentPoint < 17 ) {
+        drawCard()
+        opponentHand = hand
+        opponentPoint = result
     }
+    
+  // -------------------------------------------------
+
+  // check player total
+  let response = "";
+  playerPoint < 22 ? response = prompt("responda").toLowerCase().slice(0, 1) : console.log("fail");
+
+  while ( response === 's' ) {
+      hand = [];
+      result = 0;
+      drawCard();
+      playerHand += hand;
+      playerHand = playerHand.replaceAll(",", "");
+      playerPoint += result;
+      alert(`Player mão final: ${playerHand.toString().replaceAll(',', '')} \nTotal: ${playerPoint}`);
+      response = ''
+      playerPoint < 22 ? response = prompt("responda").toLowerCase().slice(0, 1) : alert("Estouro");
   }
 
-  // sum the result --------------------------------------
-  for (i in playerHand) {
-    if (playerHand[i] === "A") {
-      totalPlayer += 11;
-    } else if (
-      playerHand[i] === "J" ||
-      playerHand[i] === "K" ||
-      playerHand[i] === "Q"
-    ) {
-      totalPlayer += 10;
-    } else if (typeof playerHand[i] === "number") {
-      totalPlayer += playerHand[i];
-    }
-  }
+console.log('-------------------------------')
 
-  for (i in opponentHand) {
-    if (opponentHand[i] === "A") {
-      totalOponent += 11;
-    } else if (
-      opponentHand[i] === "J" ||
-      opponentHand[i] === "K" ||
-      opponentHand[i] === "Q"
-    ) {
-      totalOponent += 10;
-    } else if (typeof opponentHand[i] === "number") {
-      totalOponent += opponentHand[i];
-    }
-  }
-
-  if (playerHand[0] === "A" && playerHand[1] === "A") {
-    totalPlayer = 12;
-  }
-
-  if (opponentHand[0] === "A" && opponentHand[1] === "A") {
-    totalOponent = 12;
-  }
-  // ------------------------------------------
-
-  //   push suits to a array
-  playerHand.splice(1, 0, playerSuitToPush[0]);
-  playerHand.splice(3, 0, playerSuitToPush[1]);
-  opponentHand.splice(1, 0, opponentSuitToPush[0]);
-  opponentHand.splice(3, 0, opponentSuitToPush[1]);
-
-  playerHand = playerHand.toString().replaceAll(",", "");
-  opponentHand = opponentHand.toString().replaceAll(",", "");
-
-  console.log(`Sua mão ${playerHand} \nTotal : ${totalPlayer} Pontos`);
-  console.log(
-    `A mão do opponent ${opponentHand} \nTotal : ${totalOponent} Pontos`
-  );
-
-  if (totalOponent > totalPlayer) {
-    console.log("Oponente venceu");
-  } else if (totalOponent < totalPlayer) {
-    console.log("Jogador venceu");
+  alert(`Player mão final: ${playerHand.toString().replaceAll(',', '')} \nTotal: ${playerPoint}`);
+  alert(`Oponente mão final: ${opponentHand.toString().replaceAll(',', '')} \nTotal: ${opponentPoint}`);
+  
+  if (playerPoint === opponentPoint) {
+    alert('Empate')
+  } else if (playerPoint < opponentPoint) {
+    alert('Perdeu')
   } else {
-    console.log("Impate");
+    alert('Ganhou')
   }
 };
 
-start();
+let startResponse = prompt("Quer jogar?").toLowerCase().slice(0, 1)
+while ( startResponse === 's') {
+  start();
+  startResponse = prompt("Quer jogar denovo?").toLowerCase().slice(0, 1)
+}
